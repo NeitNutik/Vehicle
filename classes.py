4be525dafc3524640d44d3bdc4c7061b23bcf8d6
vitkovskijnikita43@gmail.com <vitkovskijnikita43@gmail.com>
2024-03-28 12:30:53 +0200

    first commit

---
+++ classes.py
@@ -0,0 +1,20 @@
+
+
+
+class Vehicle:
+      def __init__(self,make,model,year) -> None:
+          self.make = make
+          self.model = model
+          self.year = year
+      class Car:
+        make = "Germany"
+        model = "BMW M5 F90"
+        year = 2018
+        num_doors = 4
+      class Motorcycle:
+        make = "Germany"
+        model = "S1000"
+        year = 2023
+        engine_size = 3.0
+
+    
---
+++ main.py
@@ -0,0 +1,6 @@
+import classes
+
+test = classes.Vehicle.Car 
+print(test.make,test.model,test.year,test.num_doors)
+test = classes.Vehicle.Motorcycle 
+print(test.make,test.model,test.year,test.engine_size)
